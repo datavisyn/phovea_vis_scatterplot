ffd7574b6e9dde966c690e3de36f94c93019ac49
Samuel Gratzl <samuel_gratzl@gmx.at>
2016-12-22 00:38:26 +0100

    select the two cells

--- src/index.ts
+++ src/index.ts
@@ -9,8 +9,8 @@ import {IMatrix} from 'phovea_core/src/matrix';
 import {Range, Range1D} from 'phovea_core/src/range';
 
 
-export declare type NumberImplOptions = ImplOptions<[number,number]>;
-declare type NumberImpl = Impl<[number,number]>;
+export declare type NumberImplOptions = ImplOptions<[number, number]>;
+declare type NumberImpl = Impl<[number, number]>;
 
 export interface IScatterPlotOptions {
   scale?: [number, number];
@@ -41,7 +41,7 @@ export default class ScatterPlot extends AVisInstance implements IVisInstance {
 
   readonly node: HTMLElement;
   private impl: NumberImpl;
-  private loaded: [number,number][];
+  private loaded: [number, number][];
 
 
   constructor(public readonly data: IMatrix, parent: Element, options: IScatterPlotOptions = {}) {
@@ -73,8 +73,9 @@ export default class ScatterPlot extends AVisInstance implements IVisInstance {
   private onSelectionChanged() {
     const selection = this.impl.selection;
     const indices = selection.map((s) => this.loaded.indexOf(s));
-    // select whole row
-    this.data.select(new Range([Range1D.from(indices), Range1D.from([this.options.xcol, this.options.ycol])]));
+    // select the two cells
+    this.data.selectProduct([new Range([Range1D.from(indices), Range1D.single(this.options.xcol)]),
+      new Range([Range1D.from(indices), Range1D.single(this.options.ycol)])]);
   }
 
   private onDataSelectionChanged(event: any, selected: Range) {
@@ -93,11 +94,11 @@ export default class ScatterPlot extends AVisInstance implements IVisInstance {
     const node = parent.ownerDocument.createElement('div');
     parent.appendChild(node);
     node.classList.add('phovea-vis-scatterplot');
-    node.style.width = width+'px';
-    node.style.height = height+'px';
+    node.style.width = width + 'px';
+    node.style.height = height + 'px';
     const selectedColumns: Range = toRange(this.options.xcol, this.options.ycol);
 
-    Promise.all<any[]>([data.data(selectedColumns), data.cols(new Range([selectedColumns.dim(1)]))]).then(([rows, cols]: [[number,number][], string[]]) => {
+    Promise.all<any[]>([data.data(selectedColumns), data.cols(new Range([selectedColumns.dim(1)]))]).then(([rows, cols]: [[number, number][], string[]]) => {
       this.loaded = rows;
       this.impl = new Impl(rows, node, this.createImplOptions(options, cols));
 
